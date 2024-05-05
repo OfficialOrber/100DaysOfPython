a087d93e834cd6be9fffde992bd1a8dee74e2d89
Orber Jesus <ojesus@orberandco.com>
2024-05-06 00:42:23 +0100

    Formatted with pycharm

--- HigherorLower/main.py
+++ HigherorLower/main.py
@@ -1,13 +1,16 @@
 from art import logo, vs
 from game_data import data
 from random import choice
-from os import system,name
+from os import system, name
+
 
 def cls():
-    system('cls' if name=='nt' else 'clear')
+    system('cls' if name == 'nt' else 'clear')
+
+
 lives = 1
 score = 0
-while(lives > 0):
+while lives > 0:
     print(logo)
     if score > 0:
         print(f"You're right! Current score: {score}.")
@@ -19,12 +22,15 @@ while(lives > 0):
     print(vs)
     print(f'Compare B: {option_b["name"]}, a {option_b["description"]}, from {option_b["country"]}.')
     player_choice = input('Who has more followers? Type "A" or "B": ').lower()
-    right_choice = "a" if option_a['follower_count'] > option_b['follower_count'] else "b" if option_a['follower_count'] < option_b['follower_count'] else 'draw'
+    if option_a['follower_count'] > option_b['follower_count']:
+        right_choice = "a"
+    else:
+        right_choice = "b" if option_a['follower_count'] < option_b['follower_count'] else 'draw'
     if player_choice == right_choice or right_choice == 'draw':
-        score+=1
+        score += 1
         cls()
     else:
         cls()
         print(logo)
         print(f"Sorry, that's wrong. Final score: {score}")
-        lives -=1
+        lives -= 1
