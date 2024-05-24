4d7d097bde1949c8e69049b03198861630d6d6f3
Orber Jesus <ojesus@orberandco.com>
2024-05-24 02:19:59 +0100

    Finished Snake Game

---
+++ TheSnakeGame/main.py
@@ -0,0 +1,44 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # Collision with food
+    if snake.snake_head.distance(food) < 15:
+        food.refresh()
+        scoreboard.update_score()
+        snake.grow()
+    # Collision with wall
+    if snake.snake_head.xcor() > 290 or snake.snake_head.xcor() < -290 or snake.snake_head.ycor() > 290 or snake.snake_head.ycor() < -290:
+        game_is_on = False
+        scoreboard.game_over()
+    # Detect collision
+    for segment in snake.segments[1:]:
+        if snake.snake_head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+
+screen.exitonclick()
